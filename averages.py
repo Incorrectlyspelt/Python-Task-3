4f487580d0089610753ec72ba440b6aa82b26bbb
Incorrectlyspelt <Incorrectlyspelt@users.noreply.github.com>
2016-01-28 19:28:28 +0000

    Updates and code tidy

--- averages.py
+++ averages.py
@@ -1,15 +1,8 @@
 #NOW FOR AVERAGERS
 import string
-import csv#f8
 def main():
-    row_printed=0
-    row_count=-1
-    alpha_pos=0#set loop counter to zero
-    alphabet=list(string.ascii_lowercase) #get lowercase alphabet
-    file=open("class1.csv")#open the csv file : f8
-   # file=csv.reader(file,delimiter=",")#read the csv file
+    file=open("class1.csv","a")#open the csv file : f8
     print(file)
-    file=open("class1.csv","ar")#open the csv file : f8
     for line in file:
         split_line_commas=line.split(",")
         name=split_line_commas[0]
@@ -27,19 +20,6 @@ def main():
         split_line_commas=line.split(",")
         name=split_line_commas[4]
         name=split_line_commas[0]
-        
-##
-##    while row_count != row_printed:#while the single alphabet is not equal to z - last letter in the alphabet
-##        for line in file:
-##            listed_line=list(line)
-##            print(listed_line)
-####                print (line)
-####                row_printed=+1
-####            single_alphabet=alphabet[alpha_pos]
-####            #FINISHED!!!!!!!!!!!!
-##    print(row_count,row_printed)
-##    ValueError()
-##    exit
 
 if __name__ == '__main__':
     main()
