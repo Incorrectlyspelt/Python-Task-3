a0ec6f760566f9deb6d0167448b4f8ab4cd8e9e1
Incorrectlyspelt <Incorrectlyspelt@users.noreply.github.com>
2016-02-02 11:50:12 +0000

    Added todays code
    Need to merge edits with master

--- averages.py
+++ averages.py
@@ -2,25 +2,21 @@
 import string
 import csv#f8
 def main():
-    row_printed=0
-    row_count=-1
-    alpha_pos=0#set loop counter to zero
-    alphabet=list(string.ascii_lowercase) #get lowercase alphabet
+def main():
     file=open("class1.csv")#open the csv file : f8
-   # file=csv.reader(file,delimiter=",")#read the csv file
-    print(file)
-    file=open("class1.csv","ar")#open the csv file : f8
     for line in file:
         split_line_commas=line.split(",")
         name=split_line_commas[0]
         score1=split_line_commas[1]
         score2=split_line_commas[2]
         score3=split_line_commas[3]
+        remove_line_ender=score3.split(" ")
+        score3=remove_line_ender[0:1]
         total_of_scores=str(score1)+str(score2)+str(score3)
         average=(total_of_scores%3)
         print(total_of_scores)
         print(average)
-##
+
 ##    while row_count != row_printed:#while the single alphabet is not equal to z - last letter in the alphabet
 ##        for line in file:
 ##            listed_line=list(line)
