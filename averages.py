e9011ea3959d8e62c8ac8b3dc42022a44348204b
Incorrectlyspelt <Incorrectlyspelt@users.noreply.github.com>
2016-02-10 15:54:11 +0000

    School updates

--- averages.py
+++ averages.py
@@ -1,51 +1,33 @@
 #NOW FOR AVERAGERS
-import string
-def main():
-<<<<<<< HEAD
-    file=open("class1.csv","a")#open the csv file : f8
-    print(file)
-=======
-def main():
     file=open("class1.csv")#open the csv file : f8
->>>>>>> refs/remotes/origin/master
+
     for line in file:
         split_line_commas=line.split(",")#split line by commas
-        name=split_line_commas[0]#gets name from list and stores as a variable 
+        name=split_line_commas[0]#gets name from list and stores as a variable
         score1=split_line_commas[1]
         score2=split_line_commas[2]
         score3=split_line_commas[3]
-<<<<<<< HEAD
-        total_of_scores=str(score1)+str(score2)+str(score3)#Total of scores for dividing by 3 
-        average=(total_of_scores%3)#divide total of 3 scores by 3 to create an average score
-        print(total_of_scores)#dev
-        print(average)#dev
-        name=split_line[4]
-        average=split_line[0]
-    print(sorted(file))
-    for line in file:
-        split_line_commas=line.split(",")
-        name=split_line_commas[0]
-        split_line_commas[4]=""
-=======
-        remove_line_ender=score3.split(" ")
-        score3=remove_line_ender[0:1]
-        total_of_scores=str(score1)+str(score2)+str(score3)
-        average=(total_of_scores%3)
-        print(total_of_scores)
+        listed_score=list(score3)
+        score3=listed_score[0]
+        total_of_scores=int(score1)+int(score2)+int(score3)#Total of scores for dividing by 3
+        average=total_of_scores/3
         print(average)
+        line=format(score1,score2,score3,name)
+        print(line)
+        file.write(format(score1,score2,score3,name))#rewrite the line in the new formatted way
+    print(sorted(file))
+##
+##    print(sorted(file))
+##    for line in file:
+##        split_line_commas=line.split(",")#split by commas
+##        score1=split_line_commas[1]#get scores
+##        score2=split_line_commas[2]
+##        score3=split_line_commas[3]
+##        name=split_line_commas[4]#get name
+##        blank=split_line_commas[4]=""#clear column 4
+##        file.write[name,score1,score2,score3,blank]
 
-##    while row_count != row_printed:#while the single alphabet is not equal to z - last letter in the alphabet
-##        for line in file:
-##            listed_line=list(line)
-##            print(listed_line)
-####                print (line)
-####                row_printed=+1
-####            single_alphabet=alphabet[alpha_pos]
-####            #FINISHED!!!!!!!!!!!!
-##    print(row_count,row_printed)
-##    ValueError()
-##    exit
->>>>>>> refs/remotes/origin/master
+
 
 #if __name__ == '__main__':
 main()
