3490c2c7a85ed2a6b6f564cb6bdc40ff46bd906d
Incorrectlyspelt <Incorrectlyspelt@users.noreply.github.com>
2016-02-11 20:42:39 +0000

    moar code

--- averages.py
+++ averages.py
@@ -7,21 +7,17 @@ def main():
         score1=split_line_commas[1]#get scores
         score2=split_line_commas[2]
         score3=split_line_commas[3]
-        remove_line_ender=score3.split(" ")
-        score3=remove_line_ender[0:1]
-        total_of_scores=str(score1)+str(score2)+str(score3)
-        average=(total_of_scores%3)
-        print(total_of_scores)
         listed_score=list(score3)
         score3=listed_score[0]
         total_of_scores=int(score1)+int(score2)+int(score3)#Total of scores for dividing by 3
         average=total_of_scores/3
         print(average)
-        line=format(score1,score2,score3,name)
+        line=(average,score1 ,score2,score3+name)
         print(line)
-        file.write(format(score1,score2,score3,name))#rewrite the line in the new formatted way
-    print(sorted(file))
-##
+##        file.writeline([average,score1,score2,score3,name])
+##        file.write(format(score1,score2,score3,name))#rewrite the line in the new formatted way
+##    print(sorted(file))
+####
 ##    print(sorted(file))
 ##    for line in file:
 ##        split_line_commas=line.split(",")#split by commas
@@ -34,5 +30,5 @@ def main():
 
 
 
-#if __name__ == '__main__':
-main()
+if __name__ == '__main__':
+    main()
